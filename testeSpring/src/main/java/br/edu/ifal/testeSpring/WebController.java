d6f92c85946e4ab1b5c1fea35ad2c0bcf183584c
unknown <ykaroalexandre@hotmail.com>
2023-08-29 21:40:34 -0300

    Updating pom.xml

--- testeSpring/src/main/java/br/edu/ifal/testeSpring/WebController.java
+++ testeSpring/src/main/java/br/edu/ifal/testeSpring/WebController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
